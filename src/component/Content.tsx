ab2cf2dc96c5fef5ce2011278f715dc7cc8675e4
devjiin <2ddung@naver.com>
2024-12-19 00:20:43 +0900

    nav 컴포넌트 타입스크립트 오류 수정중

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -1,67 +1,78 @@
 import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
-import Nav from './Nav';
-import Section1 from './Section1';
-import Section2 from './Section2';
-import Section3 from './Section3';
-import Section4 from './Section4';
-import Section5 from './Section5';
-import Section6 from './Section6';
-import Section7 from './Section7';
-import Section8 from './Section8';
+import Nav from "./Nav";
+import Section1 from "./Section1";
+import Section2 from "./Section2";
+import Section3 from "./Section3";
+import Section4 from "./Section4";
+import Section5 from "./Section5";
+import Section6 from "./Section6";
+import Section7 from "./Section7";
+import Section8 from "./Section8";
 
-export interface INavProps {
-	scrollY: any,
-	toElement:any,
-	
+interface INavProps {
+  scrollYPos: number;
+  toElement: {
+    scrollRef: React.RefObject<HTMLDivElement>;
+    onMoveToElement: () => void;
+  };
 }
 
 const useScroll = () => {
-	const [scrollY, setScrollY] = useState<number>(0);
+  const [scrollY, setScrollY] = useState<number>(0);
 
-	const handleScroll = () => {
-		setScrollY(window.scrollY);
-	}
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
 
-	const delay = 15;
+  const delay = 15;
 
-	useEffect(() => {
-		window.addEventListener('scroll', debounce(handleScroll, delay));
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
-	return {scrollY};
-}
+  useEffect(() => {
+    window.addEventListener("scroll", debounce(handleScroll, delay));
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return { scrollY };
+};
 
-const useRefScroll = () =>{
-	const scrollRef = useRef<HTMLDivElement>(null);
-	const onMoveToElement = () => {
-		scrollRef.current?.scrollIntoView({behavior: 'smooth'});
-		// scrollRef.current?.scrollTo(100, 0);
-	};
-	return {scrollRef, onMoveToElement};
-}
-
-function Content(){
-	const scrollY = useScroll();
-	const nav = {
-		0: useRefScroll(),
-		1: useRefScroll(),
-		2: useRefScroll(),
-		3: useRefScroll(),
-	};
-	return(
-		<>
-			<Nav scrollY={scrollY} toElement={nav} />
-			<Section1 scrollRef={nav[0].scrollRef} />
-			<Section2 />
-			<Section3 />
-			<Section4 />
-			<Section5 scrollRef={nav[1].scrollRef} />
-			<Section6 scrollRef={nav[2].scrollRef} />
-			<Section7 scrollRef={nav[3].scrollRef} />
-			<Section8 />
-		</>
-	)
+function Content({ scrollYPos, toElement }: INavProps): JSX.Element {
+  const useRefScroll = (): {
+    scrollRef: React.RefObject<HTMLDivElement>;
+    onMoveToElement: () => void;
+  } => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const onMoveToElement = () => {
+      scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+      // scrollRef.current?.scrollTo(100, 0);
+    };
+    return { scrollRef, onMoveToElement };
+  };
+  const scrollYPos = useScroll();
+  const nav: Record<
+    number,
+    {
+      scrollRef: React.RefObject<HTMLDivElement>;
+      onMoveToElement: () => void;
+    }
+  > = {
+    0: useRefScroll(),
+    1: useRefScroll(),
+    2: useRefScroll(),
+    3: useRefScroll(),
+  };
+  //   console.log(scrollY, nav);
+  return (
+    <>
+      <Nav scrollYPos={scrollYPos} toElement={nav} />
+      <Section1 scrollRef={nav[0].scrollRef} />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 scrollRef={nav[1].scrollRef} />
+      <Section6 scrollRef={nav[2].scrollRef} />
+      <Section7 scrollRef={nav[3].scrollRef} />
+      <Section8 />
+    </>
+  );
 }
 
 export default Content;
--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,67 +1,86 @@
 import { useRef } from "react";
 import styled from "styled-components";
 import dummyData from "./data/data.json";
-import type { INavProps } from './Content';
+// import type { INavProps } from "./Content";
 
 const TabNav = styled.ul`
-	display: flex;
-	z-index: 100;
-	width: 100%;
-	height: 70px;
-	background-color: #202843;
-	justify-content: center;
-	&.tab__navigation--fixed{
-		position: fixed;
-		top: 72px;
-	}
-`
+  display: flex;
+  z-index: 100;
+  width: 100%;
+  height: 70px;
+  background-color: #202843;
+  justify-content: center;
+  &.tab__navigation--fixed {
+    position: fixed;
+    top: 72px;
+  }
+`;
 
 const ItemAnchor = styled.a`
-	display: block;
-	width: 240px;
-	height: 70px;
-`
+  display: block;
+  width: 240px;
+  height: 70px;
+`;
 
-function Nav({scrollY, toElement}:INavProps){
-	const navRef = useRef<(HTMLAnchorElement | null)[]>([]);
-	const headerH = 595;
-	const navH = 71;
-	
-	const siblings = function(t : any){
-		let children = t.parentElement.children;
-		let arr = [];
+export interface INavProps {
+  scrollY: number;
+  toElement: Record<
+    number,
+    {
+      scrollRef: React.RefObject<HTMLDivElement>;
+      onMoveToElement: () => void;
+    }
+  >;
+}
 
-		for(let i=0;i < children.length; i++){
-			arr.push(children[i]);
+function Nav({ props }: { props: INavProps }) {
+  const navRef = useRef<(HTMLAnchorElement | null)[]>([]);
+  const headerH = 595;
+  const navH = 71;
 
-		}
-		return arr.filter((e) => {
-			return e !== t;
-		})
+  const siblings = function (t: any) {
+    let children = t.parentElement.children;
+    let arr = [];
 
-	}
-	
-	const handleClick = (e : React.MouseEvent<HTMLAnchorElement>, idx:number) => {
-		e.preventDefault();
-		siblings(e.currentTarget.parentElement).forEach(element => {
-			element.querySelector('a').classList.remove("active");
-		});
-		e.currentTarget.classList.add("active");
-		toElement[idx].onMoveToElement();
-	}
-	return(
-		<div className="box__navigation--category">
-			<TabNav className={scrollY >= (headerH - navH) ? `tab__navigation--fixed` : ''}>
-				{dummyData.tabNavigation.map((item, idx) => 
-					<li className={`list-item${idx+1}`} key={idx}>
-						<ItemAnchor ref={(el) => navRef.current[idx] = el} href="" className="button sprite__expedia" onClick={(e)=>handleClick(e, idx)}>
-							<span className="for-a11y">{item}</span>
-						</ItemAnchor>
-					</li>
-				)}
-			</TabNav>
-		</div>
-	)
+    for (let i = 0; i < children.length; i++) {
+      arr.push(children[i]);
+    }
+    return arr.filter((e) => {
+      return e !== t;
+    });
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, idx: number) => {
+    e.preventDefault();
+    siblings(e.currentTarget.parentElement).forEach((element) => {
+      element.querySelector("a").classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    // toElement[idx].onMoveToElement();
+    console.log(props.toElement);
+  };
+  return (
+    <div className="box__navigation--category">
+      <TabNav
+        className={
+          props.scrollY >= headerH - navH ? `tab__navigation--fixed` : ""
+        }
+      >
+        {dummyData.tabNavigation.map((item, idx) => (
+          <li className={`list-item${idx + 1}`} key={idx}>
+            <ItemAnchor
+              ref={(el) => (navRef.current[idx] = el)}
+              href=""
+              className="button sprite__expedia"
+              onClick={(e) => handleClick(e, idx)}
+            >
+              <span className="for-a11y">{item}</span>
+            </ItemAnchor>
+          </li>
+        ))}
+      </TabNav>
+    </div>
+  );
 }
 
 export default Nav;
